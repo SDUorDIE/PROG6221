712ac7c45c962203c955d1b358688b08ac3368a3
SDUorDIE <zondisduduzo003@gmail.com>
2026-06-22 17:56:40 +0200

    upgraded taskitem.cs and added comments

--- PROGPART2/Models/TaskItem.cs
+++ PROGPART2/Models/TaskItem.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 
 namespace CybersecurityChatbotGUI.Models
 {
+    //Represents a cybersecurity task
     public class TaskItem
     {
+        //Unique identifier
         public int Id { get; set; }
 
+        //Task title
         public string Title { get; set; }
 
+        //Task description
         public string Description { get; set; }
 
+        //Reminder date
         public DateTime ReminderDate { get; set; }
 
+        //Completion status
         public bool IsCompleted { get; set; }
+
+        //Creation timestamp
+        public DateTime CreatedDate { get; set; }
     }
 }
